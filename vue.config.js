2b5a6902a6043bced3ff54aedd7e2e03e840339e
zhaowd <202861616@qq.com>
2022-11-02 15:40:23 +0800

    打包到docs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  // publicPath:  process.env.NODE_ENV === 'production'? '': '/', // 公共路径(必须有的)
+  outputDir: "docs", // 输出文件目录
+  assetsDir: "static"//静态资源文件名称
 };
